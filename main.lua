ad6904b239cd7b0806307fa8ea8afb462047ecf0
devapromix <maxwof@ukr.net>
2024-09-20 09:05:11 +0300

    Допиляв головний файл.

--- main.lua
+++ main.lua
@@ -1,10 +1,40 @@
 require "import"
 
+function love.load()
+	math.randomseed(os.time())
+	love.graphics.setDefaultFilter("nearest", "nearest")
+	love.window.setVSync(1)
 
+	font.load()
 
+	scene.switch('menu')
+end
 
+function love.mousepressed(x, y, button, istouch, presses)
+	scene.mousedown[button] = true
+end
 
+function love.mousereleased(x, y, button, istouch, presses)
+	scene.mousedown[button] = false
+end
 
+function love.keypressed(key, scancode, isrepeat)
+
+end
+
+function love.update(dt)
+	scene.current:update(dt)
+end
+
+function love.draw()
+	love.graphics.push()
+
+	love.graphics.pop()
+end
+
+function love.quit()
+	love.audio.stop()
+end
 
 
 
