5cc9597e751201938d5b88f77ca1f2edf0a971c5
devapromix <maxwof@ukr.net>
2024-09-25 22:16:42 +0300

    Накидав каркас класу веж.

--- game/tower.lua
+++ game/tower.lua
@@ -1,26 +1,46 @@
-local tower = {}
+local bullet = require("game.bullet")
 
+towerTypes = {}
 
+local tower = {
+	ident = 1
+}
 
+function tower:new(type, position)
+	local _tower = {}
 
+    return setmetatable(_tower, self)
+end
 
+function tower:getImage()
 
+end
 
+function tower:serialize()
+	local _tower = {}
+	
+	return _tower
+end
 
+function tower:deserialize(de)
+	local _tower = {}
 
+    self.__index = self
+    return setmetatable(_tower, self)
+end
 
+function tower:turnToTarget()
 
+end
 
+function tower:update(state, dt)
 
+end
 
-
-
-
-
-
-
-
-
-
+function tower:draw(state, x, y)
+    for _, _bullet in pairs(state.bullets) do
+        _bullet:draw()
+    end
+end
 
 return tower
